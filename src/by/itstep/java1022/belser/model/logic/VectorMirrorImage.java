053aed41aa0a452932360141b1cde0fd53156495
SergeyBelyaev <belyaevsergeyy@rambler.ru>
2023-06-10 21:20:46 +0300

    Task 02(vectorMirrorImage) is done

--- src/by/itstep/java1022/belser/model/logic/VectorMirrorImage.java
+++ src/by/itstep/java1022/belser/model/logic/VectorMirrorImage.java
@@ -2,7 +2,20 @@ package by.itstep.java1022.belser.model.logic;
 
 public class VectorMirrorImage {
     public static String mirrorImage(int[] vector){
+        int x = vector.length / 2;
+        int middle = vector[x];
+        int middleMirror = vector[x - 1];
         String msg = "Vector is mirrored";
+        for (int i = 0; i < x; i++) {
+            if (middle != middleMirror){
+                msg = "Vector is not mirrored";
+                break;
+            }
+            middle = vector[x + i];
+            middleMirror = vector[x - i - 1];
+
+        }
+
         return msg;
     }
 }
--- test/by/itstep/java1022/belser/model/logic/VectorMirrorImageTest.java
+++ test/by/itstep/java1022/belser/model/logic/VectorMirrorImageTest.java
@@ -9,23 +9,25 @@ public class VectorMirrorImageTest {
     public void vectorMirrorImage(){
 
         String expected = "Vector is not mirrored";
-        double[] vector = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        String actual = VectorSort.sort(vector);
+        int[] vector = {1, 2, 3, 4, 6, 7, 8, 9};
+        String actual = VectorMirrorImage.mirrorImage(vector);
 
         assertEquals(expected, actual);
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{1, 3, 4, 4, 4, 3, 1}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{1, 2, 3, 4, 3, 2, 1}));
-        assertEquals("Vector is not mirrored", VectorSort.sort(new double[]{9, 4, 5, 6, 6, 8, 6}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{-2, -5, -4, 3, -4, -5, -2}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{11, 32, 44, 44, 32, 11}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{-1, 3, 2, 33, 2, 3, -1}));
-        assertEquals("Vector is not mirrored", VectorSort.sort(new double[]{1, 3, 5, 10, 60, 81, 82}));
-        assertEquals("Vector is not mirrored", VectorSort.sort(new double[]{12, 13, 15, 33, 62, 81, 67}));
-        assertEquals("Vector is not mirrored", VectorSort.sort(new double[]{15, 32, 41, -3, -6, -8, -6}));
-        assertEquals("Vector is not mirrored", VectorSort.sort(new double[]{3, 2, 1, -3, -6, -8, -6}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{-9, -8, -7, -6, -7, -8, -9}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{-9, -85, -7, -6, -7, -85, -9}));
-        assertEquals("Vector is mirrored", VectorSort.sort(new double[]{-1, -2, -5, -5, -5, -2, -1}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{1, 3, 4, 4, 3, 1}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{1, 2, 3, 4, 4, 3, 2, 1}));
+        assertEquals("Vector is not mirrored", VectorMirrorImage.mirrorImage(new int[]{9, 4, 5, 6, 6, 8, 6, 1}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{-2, -5, -4, 3, 3, -4, -5, -2}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{11, 32, 44, 44, 32, 11}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{-1, 3, 2, 33, 33, 2, 3, -1}));
+        assertEquals("Vector is not mirrored", VectorMirrorImage.mirrorImage(new int[]{1, 3, 5, 10, 60, 81, 82, 23}));
+        assertEquals("Vector is not mirrored", VectorMirrorImage.mirrorImage(new int[]{12, 13, 15, 33, 33, 62, 81, 67}));
+        assertEquals("Vector is not mirrored", VectorMirrorImage.mirrorImage(new int[]{15, 32, 41, -3, -6, -6, -8, -6}));
+        assertEquals("Vector is not mirrored", VectorMirrorImage.mirrorImage(new int[]{3, 2, 2, 1, -3, -6, -8, -6}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{-9, -8, -7, -6, -6, -7, -8, -9}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{-9, -85, -7, -6, -6, -7, -85, -9}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{-1, -2, -5, -5, -5, -5, -2, -1}));
+        assertEquals("Vector is mirrored", VectorMirrorImage.mirrorImage(new int[]{-1, -2, -5, 4, 4, -5, -2, -1}));
+        assertEquals("Vector is not mirrored", VectorMirrorImage.mirrorImage(new int[]{-1, -2, -5, 4, -4, -5, -2, -1}));
 
     }
 }
